f070dee98bfdc95f8947510dcc69baef504f1356
Frikilinux <frikilinux@gmail.com>
2023-01-23 18:18:56 -0300

    Add render functions

--- index.js
+++ index.js
@@ -1,9 +1,74 @@
+const albumsContainer = document.querySelector('.container--albums');
+
 let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 const saveLocalStorage = (cartList) => {
   localStorage.setItem('cart', JSON.stringify(cartList));
 };
 
-const init = () => {};
+const renderAlbum = (album) => {
+  const {
+    id,
+    name,
+    artist,
+    totalTracks,
+    price,
+    albumImg,
+    releaseDate,
+    genre,
+    label,
+  } = album;
+  return `<div class="albums__release">
+  <img src="${albumImg}" class="release-img" alt="Album image">
+  <div class="format">
+    <div class="img-select trans-5">
+      <img src="./assets/img/dsd_logo.svg" alt="" class="format-img">
+    </div>
+    <div class="img-select trans-5">
+      <img src="./assets/img/flac_logo.svg" alt="" class="format-img">
+    </div>
+    <div class="img-select trans-5">
+      <img src="./assets/img/mqa_logo.svg" alt="" class="format-img">
+    </div>
+  </div>
+  <div class="release__info">
+    <div class="release__data">
+      <p class="album-name">${name}</p>
+      <p class="artist">${artist}</p>
+      <p class="tracks">${totalTracks} pistas</p>
+    </div>
+    <div class="release__price">
+      <p class="price">$ ${price}</p>
+      <button
+        class="btn--buy btn"
+        data-id="${id}"
+        data-artist="${artist}"
+        data-name="${name}"
+        data-bid="${price}"
+        data-img="${albumImg}">
+          Comprar
+      </button>
+    </div>
+  </div>
+</div>`;
+};
+
+// Renderizado de albumes
+const renderDividedAlbums = (i = 0) => {
+  albumsContainer.innerHTML += productsController.dividedAlbums[i]
+    .map(renderAlbum)
+    .join('');
+};
+
+const renderFilterdAlbum = (genre) => {
+  const albumList = albumsData.filter((album) => {
+    return album.genre === genre
+  })
+  albumsContainer.innerHTML = albumList.map(renderAlbum).join('')
+}
+
+const init = () => {
+  renderDividedAlbums()
+};
 
 init();
