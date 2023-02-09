9b8d4197f2f9309c3bc6579af362b1bf9cb6973f
Frikilinux <frikilinux@gmail.com>
2023-02-08 23:37:12 -0300

    Clean code

--- script/main.js
+++ script/main.js
@@ -26,33 +26,6 @@ const previewbtnBuy = document.querySelector('.preview-buy');
 // const cartBtnBuy = document.querySelector('.btn-buy');
 // const cartBtnDelete = document.querySelector('.btn-delete');
 
-// // Trayendo cart guardado
-// let getUserCart = () => {
-//   return !loggedUser.length ? [] : loggedUser[0].cart;
-// };
-
-// let userDb = JSON.parse(localStorage.getItem('userDb')) || [];
-// let loggedUser = JSON.parse(localStorage.getItem('loggedUser')) || [];
-// let cart = getUserCart();
-
-// const saveUserDbStorage = (userDb) => {
-//   localStorage.setItem('userDb', JSON.stringify(userDb));
-// };
-
-// const saveLoginStorage = (loggedUser) => {
-//   localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
-// };
-
-// const updateUserDb = (loggedUser) => {
-//   userDb = userDb.map((user) => {
-//     return user.id === loggedUser[0].id ? loggedUser[0] : user;
-//   });
-// };
-
-// const updateCartOfLoggedUser = (user) => {
-//   loggedUser = user.map((user) => ({ ...user, cart: [...cart] }));
-// };
-
 const renderAlbum = (album) => {
   const {
     id,
