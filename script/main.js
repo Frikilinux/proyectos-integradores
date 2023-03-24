6f6dc84f4bf8c4eda94d9ea05789b06dca7e7659
Frikilinux <frikilinux@gmail.com>
2023-03-24 10:21:07 -0300

    Clean indent

--- script/main.js
+++ script/main.js
@@ -31,43 +31,54 @@ const albumCardTemplate = (album) => {
   const { id, name, artist, totalTracks, price, albumImg, releaseDate, label } =
     album;
   return `
-  <div id="${id}" class="albums__release">
-    <div class="back-release">
-      <div class="format">
-        <button data-id="${id}" data-name="previewMenu" data-type="btnMenu" class="btn-show-preview trans-5">Preview<i class="far fa-circle-play fa-beat" data-name="previewMenu" data-id="${id}"></i></button>
-        <div class="img-select trans-5">
-        <p>Disponible en</p>
-        <img src="./assets/img/dsd_logo.svg" alt="" class="format-img">
-        <img src="./assets/img/flac_logo.svg" alt="" class="format-img">
-        <img src="./assets/img/mqa_logo.svg" alt="" class="format-img">
+    <div id="${id}" class="albums__release">
+      <div class="back-release">
+        <div class="format">
+          <button
+            data-id="${id}"
+            data-name="previewMenu"
+            data-type="btnMenu"
+            class="btn-show-preview
+            trans-5">
+            Preview
+            <i class="far fa-circle-play fa-beat"
+              data-name="previewMenu"
+              data-id="${id}">
+            </i>
+          </button>
+          <div class="img-select trans-5">
+          <p>Disponible en</p>
+          <img src="./assets/img/dsd_logo.svg" alt="" class="format-img">
+          <img src="./assets/img/flac_logo.svg" alt="" class="format-img">
+          <img src="./assets/img/mqa_logo.svg" alt="" class="format-img">
+          </div>
+        </div>  
+        <img src="${albumImg}" class="release-img trans-5" alt="Album image">
+      </div>
+      <div class="release__info">
+        <div class="release__data">
+          <p class="album-name">${name}</p>
+          <p class="artist">${artist}</p>
+          <p class="tracks">${totalTracks} pistas - Release ${releaseDate}</p>
         </div>
-      </div>  
-      <img src="${albumImg}" class="release-img trans-5" alt="Album image">
-    </div>
-    <div class="release__info">
-      <div class="release__data">
-        <p class="album-name">${name}</p>
-        <p class="artist">${artist}</p>
-        <p class="tracks">${totalTracks} pistas - Release ${releaseDate}</p>
-      </div>
-      <div class="release__price">
-        <p class="price">$ ${price}</p>
-        <button
-          class="btn--buy btn"
-          data-id="${id}"
-          data-artist="${artist}"
-          data-name="${name}"
-          data-price="${price}"
-          data-img="${albumImg}"
-          data-tracks="${totalTracks}"
-          data-date="${releaseDate}"
-          data-label="${label}">
-            Comprar
-        </button>
+        <div class="release__price">
+          <p class="price">$ ${price}</p>
+          <button
+            class="btn--buy btn"
+            data-id="${id}"
+            data-artist="${artist}"
+            data-name="${name}"
+            data-price="${price}"
+            data-img="${albumImg}"
+            data-tracks="${totalTracks}"
+            data-date="${releaseDate}"
+            data-label="${label}">
+              Comprar
+          </button>
+        </div>
       </div>
     </div>
-  </div>
-`;
+  `;
 };
 
 // Divide albumes en arrays
